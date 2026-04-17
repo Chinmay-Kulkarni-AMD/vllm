455f7caf74982e439ea15c362968079a2a6233bc
Chinmay Kulkarni <Chinmay.Kulkarni@amd.com>
2026-04-16 22:25:41 -0600

    [ZenCPU] Update the zentorch-weekly version to latest known-good version
    Signed-off-by: Chinmay Kulkarni <Chinmay.Kulkarni@amd.com>

--- setup.py
+++ setup.py
@@ -1086,7 +1086,7 @@ setup(
     extras_require={
         # AMD Zen CPU optimizations via zentorch
         "zen": [
-            "zentorch-weekly==5.2.1.dev20260325"
+            "zentorch-weekly==5.2.1.dev20260408"
         ],  # Zentorch has weekly releases. This pulls the known-good version.
         "bench": ["pandas", "matplotlib", "seaborn", "datasets", "scipy", "plotly"],
         "tensorizer": ["tensorizer==2.10.1"],
